4179338da398403443718ae9e58e751b154be65c
alexandruale26 <90907786+alexandruale26@users.noreply.github.com>
2022-11-26 09:59:05 +0200

    Add files via upload

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alexandru Tenie on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
